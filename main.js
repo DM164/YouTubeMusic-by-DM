5b3cfc5cdb10431ba7ba4777f77ddb4ab806ec72
Denis Mignanelli <denistschurtschenthaler@gmail.com>
2020-01-30 19:28:26 +0100

    source 3.0.0

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ const { app, BrowserWindow, ipcMain, globalShortcut, shell, dialog, Tray, Menu }
 const path = require('path')
 
 // Disable error dialogs
-dialog.showErrorBox = function(title, content) {
-    console.log(`${title}\n${content}`);
+dialog.showErrorBox = function (title, content) {
+  console.log(`${title}\n${content}`);
 };
 
 //Client Version (PACKAGE.JSON HAS TO BE CHANGED MANUALLY)
 const ClientVersion = '3.0.0'
 
 //Production
-process.env.NODE_ENV='production'
+process.env.NODE_ENV = 'production'
 
 //Update the app automatically
 const { autoUpdater } = require("electron-updater")
 autoUpdater.checkForUpdatesAndNotify()
-if (autoUpdater.isUpdateAvailable){
+if (autoUpdater.isUpdateAvailable) {
   autoUpdater.quitAndInstall()
 }
 
@@ -31,9 +31,9 @@ const largeImageText = `YouTube Music App by DM (v${ClientVersion})`
 //Discord rich presence active setting switch
 let discordRichPresence = null;
 
-ipcMain.on('send-DRPstatus', function(event, arg){
+ipcMain.on('send-DRPstatus', function (event, arg) {
   discordRichPresence = arg.lel
-  if (discordRichPresence == "true"){
+  if (discordRichPresence == "true") {
     idleDRP()
   } else {
     client.disconnect()
@@ -89,19 +89,18 @@ function createSplash() {
     stylesheet.setAttribute("href", "https://dl.dropbox.com/s/sgb2h7emq0uk8y4/style.css?dl=0");
     document.querySelector('head').appendChild(stylesheet);
     `)
-      setTimeout(() => {
-        mainWindow.show()
-        splash.close();
-        splash = null;
-      }, 1000);
-
+    setTimeout(() => {
+      mainWindow.show()
+      splash.close();
+      splash = null;
+    }, 1000);
   })
   ipcMain.on('versionCheck:splash', () => {
     splash.webContents.send('versionCheckAnswer', ClientVersion)
   })
 }
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1100,
@@ -145,51 +144,74 @@ function createWindow () {
   });
 
   //Overlay commands
-  ipcMain.on('overlay-play-pause', function(){
+  ipcMain.on('overlay-play-pause', function () {
     mainWindow.webContents.send('play-pause-request')
   })
-  ipcMain.on('overlay-next', function(){
+  ipcMain.on('overlay-next', function () {
     mainWindow.webContents.send('next-track-request')
   })
-  ipcMain.on('overlay-previous', function(){
+  ipcMain.on('overlay-previous', function () {
     mainWindow.webContents.send('previous-track-request')
   })
 
-  ipcMain.on('overlay-volume-up', function(){
+  ipcMain.on('overlay-volume-up', function () {
     mainWindow.webContents.send('volume-up')
   })
-  ipcMain.on('overlay-volume-down', function(){
+  ipcMain.on('overlay-volume-down', function () {
     mainWindow.webContents.send('volume-down')
   })
 
-  ipcMain.on('overlay-close', function(){
+  ipcMain.on('overlay-close', function () {
     overlayToggle()
   })
 
-//Opens the Github page to download the latest version of the client
-ipcMain.on('openReleases', function(){
+  mainWindow.webContents.on('dom-ready', () => {
+    mainWindow.webContents.executeJavaScript(`
+        
+    const topBarJS = document.createElement('script');
+    topBarJS.setAttribute('src', 'https://dl.dropbox.com/s/dhoar8i0zkhhhd4/topBar.js?dl=0')
+    topBarJS.setAttribute('defer', '')
+    document.querySelector('body').prepend(topBarJS);
+    
+    const retrieveServerData = document.createElement('script');
+    retrieveServerData.setAttribute('src', 'https://dl.dropbox.com/s/q45u3wo8zaqthnv/serverData.js?dl=0')
+    retrieveServerData.setAttribute('defer', '')
+    document.querySelector('body').prepend(retrieveServerData);
+    
+    const javascript = document.createElement('script');
+    javascript.setAttribute('src', 'https://dl.dropbox.com/s/y9j9n1tt2l06bsc/customscript_2.0.js?dl=0')
+    javascript.setAttribute('defer', '')
+    document.querySelector('body').prepend(javascript);
+    
+    const stylesheet = document.createElement('link');
+    stylesheet.setAttribute("rel", "stylesheet");
+    stylesheet.setAttribute("href", "https://dl.dropbox.com/s/sgb2h7emq0uk8y4/style.css?dl=0");
+    document.querySelector('head').appendChild(stylesheet);
+    `)
+  })
+
+  //Opens the Github page to download the latest version of the client
+  ipcMain.on('openReleases', function () {
     shell.openExternal('https://github.com/DM164/Unoffical-YouTube-Music-App/releases');
-});
+  });
 
 
-let songTitle = 'Title'
-let songArtist = 'Artist'
-let playerVolume = '0'
-let endTimestamp = '0:00'
-let mediaStatus =  'paused'
+  let songTitle = 'Title'
+  let songArtist = 'Artist'
+  let playerVolume = '0'
+  let endTimestamp = '0:00'
+  let mediaStatus = 'paused'
 
-//DISCORD RICH PRESENCE (new)
-  mainWindow.webContents.on('media-started-playing', function(){
+  //DISCORD RICH PRESENCE (new)
+  mainWindow.webContents.on('media-started-playing', function () {
     mediaStatus = 'playing'
 
     mainWindow.webContents.send('request-song-data')
-    ipcMain.on('requested-data', function(event, arg){
-      
+    ipcMain.on('requested-data', function (event, arg) {
+
       client.updatePresence({
         details: arg.title, //Song title
         state: arg.artist, //Artist
-        // startTimestamp: Date.now(),
-        // endTimestamp: Date.now() + 240000, //Will be added in a future version
         largeImageKey: largeImg,
         largeImageText: largeImageText,
         smallImageKey: 'play',
@@ -203,76 +225,76 @@ let mediaStatus =  'paused'
       playerVolume = arg.volume
       endTimestamp = arg.endTimestamp
       thumb = arg.thumb
-      });
+    });
 
 
-    mainWindow.webContents.on('media-paused', function(){
+    mainWindow.webContents.on('media-paused', function () {
       mediaStatus = 'paused'
 
-        client.updatePresence({
-          details: songTitle, //Song title
-          state: songArtist, //Artist
-          largeImageKey: largeImg,
-          largeImageText: largeImageText,
-          smallImageKey: 'pause',
-          smallImageText: 'Paused',
-          instance: true,
-        });
+      client.updatePresence({
+        details: songTitle, //Song title
+        state: songArtist, //Artist
+        largeImageKey: largeImg,
+        largeImageText: largeImageText,
+        smallImageKey: 'pause',
+        smallImageText: 'Paused',
+        instance: true,
+      });
     })
 
     //Overlay data request
-    ipcMain.on('request-overlay-data', function(){
+    ipcMain.on('request-overlay-data', function () {
       mainWindow.webContents.send('request-song-data')
-        let data = [
-          title = songTitle,
-          artist = songArtist,
-          volume = playerVolume,
-          endTimestamp = endTimestamp,
-          thumb = thumb,
-          mediaStatus = mediaStatus,
-          DRPStatus = clientIsConnected
-        ]
-        overlay.webContents.send('requested-overlay-data', data)
+      let data = [
+        title = songTitle,
+        artist = songArtist,
+        volume = playerVolume,
+        endTimestamp = endTimestamp,
+        thumb = thumb,
+        mediaStatus = mediaStatus,
+        DRPStatus = clientIsConnected
+      ]
+      overlay.webContents.send('requested-overlay-data', data)
     })
 
     //Volume request
-    ipcMain.on('request-volume-data', function(){
+    ipcMain.on('request-volume-data', function () {
       mainWindow.send('request-volume-data')
     })
-    ipcMain.on('requested-volume-data', function(event, arg){
+    ipcMain.on('requested-volume-data', function (event, arg) {
       overlay.send('requested-volume-data', arg)
     })
     //Timestamp request
-    ipcMain.on('request-time-data', function(){
+    ipcMain.on('request-time-data', function () {
       mainWindow.send('request-time-data')
     })
-    ipcMain.on('requested-time-data', function(event, arg){
+    ipcMain.on('requested-time-data', function (event, arg) {
       overlay.send('requested-time-data', arg)
     })
 
   })
 
 
-ipcMain.on('open-mainWindow', function(){
-  mainWindow.show();
-  overlay.close()
-  overlayOpen = false;
-});
+  ipcMain.on('open-mainWindow', function () {
+    mainWindow.show();
+    overlay.close()
+    overlayOpen = false;
+  });
 
-ipcMain.on('restart-app', function(){
-  createWarning('In order to chane this option you are going to have to restart the app.')
-});
+  ipcMain.on('restart-app', function () {
+    createWarning('In order to chane this option you are going to have to restart the app.')
+  });
 
-ipcMain.on('relaunch-app', function(){
-  createWarning('In order to change this option you are going to have to restart the app.')
-});
-ipcMain.on('relaunch-app:confirmed', function(){
-  app.relaunch();
-  app.exit();
-});
-ipcMain.on('closeApp:close', function(){
-  app.quit();
-});
+  ipcMain.on('relaunch-app', function () {
+    createWarning('In order to change this option you are going to have to restart the app.')
+  });
+  ipcMain.on('relaunch-app:confirmed', function () {
+    app.relaunch();
+    app.exit();
+  });
+  ipcMain.on('closeApp:close', function () {
+    app.quit();
+  });
 
   // App Splashscreen
   mainWindow.loadURL('https://music.youtube.com/');
@@ -295,33 +317,33 @@ const DRPIconConnect = path.join(__dirname + '/assets/tray/ConnectDRP.png')
 const openOverlayIcon = path.join(__dirname + '/assets/tray/overlay.png')
 const quitAppIcon = path.join(__dirname + '/assets/tray/close.png')
 
-app.on('ready', () =>{
+app.on('ready', () => {
   createSplash()
 
   tray = new Tray(path.join(__dirname + '/assets/tray/tray-light.png'))
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'YouTube Music by DM', type: 'normal', icon: titleIcon, enabled: false},
-    { label: 'separator', type: 'separator'},
-    { label: 'Disconnect DiscordRP', icon: DRPIconDisconnect, click: function trayfunction2() { client.disconnect(); clientIsConnected = false }},
-    { label: 'Connect DiscordRP', icon: DRPIconConnect, click: function trayfunction4() { if(clientIsConnected === false){createNewPresence(); clientIsConnected = true} }},
-    { label: 'Discord separator', type: 'separator'},
-    { label: 'Open App', type:'normal', icon: openAppIcon, click: function trayfunction1() { mainWindow.show() }},
-    { label: 'Open Overlay', accelerator: 'Alt + 1', icon: openOverlayIcon, click: function trayfunction3() { overlayToggle() }},
-    { label: 'Quit App', accelerator: 'CommandOrControl + Q', icon: quitAppIcon, click: function(){ createWarning('Do you really want to quit the app?') }}
+    { label: 'YouTube Music by DM', type: 'normal', icon: titleIcon, enabled: false },
+    { label: 'separator', type: 'separator' },
+    { label: 'Disconnect DiscordRP', icon: DRPIconDisconnect, click: function trayfunction2() { client.disconnect(); clientIsConnected = false } },
+    { label: 'Connect DiscordRP', icon: DRPIconConnect, click: function trayfunction4() { if (clientIsConnected === false) { createNewPresence(); clientIsConnected = true } } },
+    { label: 'Discord separator', type: 'separator' },
+    { label: 'Open App', type: 'normal', icon: openAppIcon, click: function trayfunction1() { mainWindow.show() } },
+    { label: 'Open Overlay', accelerator: 'Alt + 1', icon: openOverlayIcon, click: function trayfunction3() { overlayToggle() } },
+    { label: 'Quit App', accelerator: 'CommandOrControl + Q', icon: quitAppIcon, click: function () { createWarning('Do you really want to quit the app?') } }
   ])
   tray.setToolTip('YouTube Music by DM')
   tray.setContextMenu(contextMenu)
 
   //Build menu from template
-  const topMenu = Menu.buildFromTemplate(mainMenuTemplate);
-  Menu.setApplicationMenu(topMenu)
+  // const topMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Menu.setApplicationMenu(topMenu)
 })
 
 function createNewPresence() {
   client = require('discord-rich-presence')('611219815138590731');
   mainWindow.webContents.send('play-pause-request')
   client.updatePresence({
-  details: 'Not listening to anything',
+    details: 'Not listening to anything',
     largeImageKey: largeImg,
     largeImageText: largeImageText,
     smallImageKey: 'menus',
@@ -347,21 +369,21 @@ app.on('activate', function () {
 })
 
 //DISCORD RICH PRESENCE at startup
-function idleDRP(){
-    client.updatePresence({
-      details: 'Not listening to anything',
-      largeImageKey: largeImg,
-      largeImageText: largeImageText,
-      smallImageKey: 'menus',
-      smallImageText: 'In the menus',
-      instance: true,
-    });
+function idleDRP() {
+  client.updatePresence({
+    details: 'Not listening to anything',
+    largeImageKey: largeImg,
+    largeImageText: largeImageText,
+    smallImageKey: 'menus',
+    smallImageText: 'In the menus',
+    instance: true,
+  });
 }
 
 //Toggle the Overlay
 let overlayOpen = false
 function overlayToggle() {
-  if(overlayOpen == false){
+  if (overlayOpen == false) {
     createOverlay();
     overlayOpen = true;
   } else {
@@ -372,7 +394,7 @@ function overlayToggle() {
 }
 
 //Overlay window
-function createOverlay () {
+function createOverlay() {
   // Create the browser window.
   overlay = new BrowserWindow({
     width: 900,
@@ -400,11 +422,11 @@ function createOverlay () {
   })
 }
 
-ipcMain.on('open:settings', function(){
+ipcMain.on('open:settings', function () {
   createSettings()
 });
 //Settings window
-function createSettings () {
+function createSettings() {
   // Create the browser window.
   settings = new BrowserWindow({
     width: 522,
@@ -475,38 +497,38 @@ function createYTWindow() {
   })
 }
 
-ipcMain.on('versionCheck', function(){
+ipcMain.on('versionCheck', function () {
   mainWindow.webContents.send('versionCheckAnswer', ClientVersion)
 })
 
 // New top menu template (edit, view, ect.) (we used it for shortcuts), only for pruduction
 const mainMenuTemplate = [
   {
-      label:'App',
-      submenu:[
-          {
-            label: 'Quit',
-            accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q', //shortcut to quit, checks if the app is running on win32 or on darwin(MacOS)
-            click(){
-                createWarning('Do you really want to quit the app?')
-            }
-          },
-          {
-            label: 'Open YouTube',
-            accelerator: 'Alt + 3',
-            click(){
-                createYTWindow();
-            }
-          },
-          {
-            label: 'Open YouTube',
-            accelerator: 'Esc',
-            click(){
-                if(overlayOpen === true){
-                  overlayToggle()
-                }
-            }
+    label: 'App',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q', //shortcut to quit, checks if the app is running on win32 or on darwin(MacOS)
+        click() {
+          createWarning('Do you really want to quit the app?')
+        }
+      },
+      {
+        label: 'Open YouTube',
+        accelerator: 'Alt + 3',
+        click() {
+          createYTWindow();
+        }
+      },
+      {
+        label: 'Open YouTube',
+        accelerator: 'Esc',
+        click() {
+          if (overlayOpen === true) {
+            overlayToggle()
           }
-      ]
+        }
+      }
+    ]
   }
 ]
